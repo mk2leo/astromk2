ab724491cb8c27102aea78892ab001b166e24f24
mk2 <mk2leo@qq.com>
2024-10-17 13:19:15 +0800

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+
+server:{                 //此处为新增代码
+		host:'0.0.0.0',      //此处为新增代码
+	}                        //此处为新增代码
+	
+	});
